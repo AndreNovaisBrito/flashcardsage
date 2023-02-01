347bc735d0d6765c7c27b94b0de73a334871301d
AndreNovaisBrito <andrenoz@gmail.com>
2023-02-01 15:38:04 -0300

    working on setting up the ui and api, plus adding /deck endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express, {Request, Response} from "express";
+import mongoose from 'mongoose';
+import Deck from './models/Deck';
+
+const PORT = 5000;
+const app = express();
+app.use(express.json()); //Adds support to json payloads
+
+
+
+app.get('/', (req:Request, res:Response)=>{
+    res.send("GGWP");
+});
+
+app.post('/decks', async (req: Request, res: Response)=>{
+    //I tried to res.send("Hello World") and it broke the code
+    console.log(req.body);
+    const newDeck = new Deck({
+        title: req.body.title,
+    });
+    //When you save, you get an object back, so we save it on createdDeck, so we can return it to the user
+    const createdDeck = await newDeck.save(); //Save is assyncronous, so it's probably good to await, also put async in the callback function
+    res.json(createdDeck);
+});
+
+mongoose.set('strictQuery', true); //I did this to remove a warning;
+
+mongoose.connect(
+    'mongodb+srv://andrenovais:b7c4Nn8wlot75DpI@cluster0.9h0vm3q.mongodb.net/?retryWrites=true&w=majority'
+    ).then(()=>{
+        console.log(`Listening on port ${PORT}`);
+        app.listen(PORT);
+    })
