8e31b9076041c6c8ab6dbfa15ad6e923c23a2f84
AndreNovaisBrito <andrenoz@gmail.com>
2023-02-02 05:35:25 -0300

    working on showing decks on the landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,19 +3,31 @@ config();
 
 import express, {Request, Response} from "express";
 import mongoose from 'mongoose';
+import cors from 'cors';
 import Deck from './models/Deck';
 
 const PORT = 5000;
 
 const app = express();
 
+app.use(
+    cors({
+        origin: "*"
+    }));
+
 app.use(express.json()); //Adds support to json payloads
 
+app.get('/decks', async (req: Request, res: Response)=>{
 
+    //TODO: fetch all decks and send back to the user
+    // 1. how do we fetch decks from mongo?
+    const decks = await Deck.find();
+    console.log(decks);
+    // 2. how do we send back the array to the ui?
+    res.json(decks);
 
-app.get('/', (req:Request, res:Response)=>{
-    res.send("GGWP");
-});
+})
+
 
 app.post('/decks', async (req: Request, res: Response)=>{
     //I tried to res.send("Hello World") and it broke the code
@@ -34,4 +46,4 @@ mongoose.set('strictQuery', true); //I did this to remove a warning;
 mongoose.connect(process.env.MONGO_URL!).then(()=>{
         console.log(`Listening on port ${PORT}`);
         app.listen(PORT);
-    })
+    });
